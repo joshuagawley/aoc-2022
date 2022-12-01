8b2eda1cb996069035297be3db307baaf86dd844
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2022-12-01 10:41:21 +0000

    Add both part 1 and part 2 solutions
    Signed-off-by: Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,16 +2,37 @@ use std::collections::BinaryHeap;
 use anyhow::Result;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
+use std::iter::Flatten;
 
-fn main() -> Result<()> {
-    let file = File::open("input")?;
-    let reader = BufReader::new(file);
+type FlattenedLines = Flatten<Lines<BufReader<File>>>;
 
+fn open_file(input: &str) -> Result<FlattenedLines> {
+    let file = File::open(input)?;
+    Ok(BufReader::new(file).lines().flatten())
+}
+
+fn part1(lines: &mut FlattenedLines) -> Result<String> {
+    let mut max = i32::MIN;
+    let mut num = 0;
+
+    for line in lines {
+        if line.is_empty() {
+            max = max.max(num);
+            num = 0;
+        } else {
+            num += line.parse::<i32>()?;
+        }
+    }
+
+    Ok(max.to_string())
+}
+
+fn part2(lines: &mut FlattenedLines) -> Result<String> {
     let mut heap = BinaryHeap::new();
     let mut num = 0;
 
-    for line in reader.lines().flatten() {
+    for line in lines {
         if line.is_empty() {
             heap.push(num);
             num = 0;
@@ -26,7 +47,16 @@ fn main() -> Result<()> {
             result += x;
         }
     }
-    println!("{}", result);
+
+    Ok(result.to_string())
+}
+
+fn main() -> Result<()> {
+    let part1_result = part1(&mut open_file("input")?)?;
+    let part2_result = part2(&mut open_file("input")?)?;
+
+    println!("Part 1 answer: {}", part1_result);
+    println!("Part 2 answer: {}", part2_result);
 
     Ok(())
 }
