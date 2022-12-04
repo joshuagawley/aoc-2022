78f363907800b14b6633d149ad7b22f15d965217
Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>
2022-12-04 21:39:11 +0000

    Complete day 4
    Signed-off-by: Joshua Gawley <16921823+joshuagawley@users.noreply.github.com>

---
+++ day4/src/main.rs
@@ -0,0 +1,42 @@
+use common::*;
+
+fn get_ranges(line: String) -> Vec<Vec<i32>> {
+    line.split(',')
+        .map(|x| {
+            x.split('-')
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+fn range_fully_contained(range: Vec<Vec<i32>>) -> bool {
+    (range[0][0] <= range[1][0] && range[1][1] <= range[0][1])
+        || (range[1][0] <= range[0][0] && range[0][1] <= range[1][1])
+}
+
+fn range_overlap(range: Vec<Vec<i32>>) -> bool {
+    i32::min(range[0][1], range[1][1]) >= i32::max(range[0][0], range[1][0])
+}
+
+fn common(input: &mut FlattenedLines, pred: &dyn Fn(Vec<Vec<i32>>) -> bool) -> i32 {
+    input.fold(0, |x, str| if pred(get_ranges(str)) { x + 1 } else { x })
+}
+
+fn part1(input: &mut FlattenedLines) -> i32 {
+    common(input, &range_fully_contained)
+}
+
+fn part2(input: &mut FlattenedLines) -> i32 {
+    common(input, &range_overlap)
+}
+
+fn main() -> anyhow::Result<()> {
+    let part1_result = part1(&mut open_file("input")?);
+    let part2_result = part2(&mut open_file("input")?);
+
+    println!("Part 1 result: {}", part1_result);
+    println!("Part 2 result: {}", part2_result);
+
+    Ok(())
+}
